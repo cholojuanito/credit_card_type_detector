325812578bf62f2fef64e2f47e057e379c1257d8
Tanner Davis <tannerdavisr@gmail.com>
2023-03-28 22:25:53 -0600

    Update pub_login.sh

--- pub_login.sh
+++ pub_login.sh
@@ -1,29 +1,39 @@
+#!/usr/bin/env bash
 # This script creates/updates credentials.json file which is used
 # to authorize publisher when publishing packages to pub.dev
+
+RED='\033[0;31m'
+
+errln() {
+    echo "❌ $RED Error: $1"
+    exit 1
+}
 
 # Checking whether the secrets are available as environment
 # variables or not.
 if [ -z "${PUB_DEV_PUBLISH_ACCESS_TOKEN}" ]; then
-  echo "Missing PUB_DEV_PUBLISH_ACCESS_TOKEN environment variable"
-  exit 1
+  errln "Missing PUB_DEV_PUBLISH_ACCESS_TOKEN environment variable"
 fi
 
 if [ -z "${PUB_DEV_PUBLISH_REFRESH_TOKEN}" ]; then
-  echo "Missing PUB_DEV_PUBLISH_REFRESH_TOKEN environment variable"
-  exit 1
+  errln "Missing PUB_DEV_PUBLISH_REFRESH_TOKEN environment variable"
 fi
 
 if [ -z "${PUB_DEV_PUBLISH_TOKEN_ENDPOINT}" ]; then
-  echo "Missing PUB_DEV_PUBLISH_TOKEN_ENDPOINT environment variable"
-  exit 1
+  errln "Missing PUB_DEV_PUBLISH_TOKEN_ENDPOINT environment variable"
 fi
 
 if [ -z "${PUB_DEV_PUBLISH_EXPIRATION}" ]; then
-  echo "Missing PUB_DEV_PUBLISH_EXPIRATION environment variable"
-  exit 1
+  errln "Missing PUB_DEV_PUBLISH_EXPIRATION environment variable"
+fi
+
+# creates directory to store creditionals
+if [ ! -d "~/.pub-cache" ]; then
+    mkdir ~/.pub-cache
 fi
 
 # Create credentials.json file.
+echo "📝 Writing credentials"
 cat <<EOF > ~/.pub-cache/credentials.json
 {
   "accessToken":"${PUB_DEV_PUBLISH_ACCESS_TOKEN}",
@@ -33,3 +43,6 @@ cat <<EOF > ~/.pub-cache/credentials.json
   "expiration":${PUB_DEV_PUBLISH_EXPIRATION}
 }
 EOF
+
+echo "🔑 Credentials checksum"
+sha1sum -b ~/.pub-cache/credentials.json
